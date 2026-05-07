465ae167d0bdbdba4e1a30d60a8c27a43e65793a
AbdelrahmanSamirz <shekaba@gmail.com>
2026-05-07 17:15:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Session04_EFcore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
